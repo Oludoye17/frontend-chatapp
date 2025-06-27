ad28d70114e9fe65cf6aa9a712ed18c43b4fe941
oludoye olumide <oludoyeolumide@gmail.com>
2025-06-27 23:54:41 +0100

    frontend-chatapp

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,6 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
+export { default as socketService } from './services/socket.ts'
